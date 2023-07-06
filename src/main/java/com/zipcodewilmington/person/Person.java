29a79d4cb022b8aba3f22dcd5d966bd547d55b69
taylor <taylormyers01@comcast.net>
2023-07-06 19:05:59 -0400

    added fields to the person class, created setters and getters, tested all functionality

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,84 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String favColor;
+    private String nickname;
 
+    public String getFavColor() {
+        return favColor;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getFavoriteNum() {
+        return favoriteNum;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    private int favoriteNum;
+    private boolean isAlive;
+
+    public void setFavColor(String favColor) {
+        this.favColor = favColor;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setFavoriteNum(int favoriteNum) {
+        this.favoriteNum = favoriteNum;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String phoneNumber;
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -95,4 +95,134 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+    public void testGetFavColor() {
+        // Given
+        Person person = new Person();
+        String expected = "red";
+
+        // When
+        person.setFavColor(expected);
+
+        // Then
+        String actual = person.getFavColor();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    public void testGetNickname() {
+        // Given
+        Person person = new Person();
+        String expected = "Bean Man";
+
+        // When
+        person.setNickname(expected);
+
+        // Then
+        String actual = person.getNickname();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testGetFavoriteNum() {
+        // Given
+        Person person = new Person();
+        Integer expected = 16;
+
+        // When
+        person.setFavoriteNum(expected);
+
+        // Then
+        Integer actual = person.getFavoriteNum();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testIsAlive() {
+        // Given
+        Person person = new Person();
+        boolean expected = false;
+
+        // When
+        person.setAlive(expected);
+
+        // Then
+        boolean actual = person.isAlive();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testGetPhoneNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "867-5309";
+
+        // When
+        person.setPhoneNumber(expected);
+
+        // Then
+        String actual = person.getPhoneNumber();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testSetFavColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Spaghetti red";
+
+        // When
+        person.setFavColor(expected);
+
+        // Then
+        String actual = person.getFavColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testSetNickname() {
+        // Given
+        Person person = new Person();
+        String expected = "Catwoman";
+
+        // When
+        person.setNickname(expected);
+
+        // Then
+        String actual = person.getNickname();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testSetFavoriteNum() {
+        // Given
+        Person person = new Person();
+        Integer expected = 45902;
+
+        // When
+        person.setFavoriteNum(expected);
+
+        // Then
+        Integer actual = person.getFavoriteNum();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testSetAlive() {
+        // Given
+        Person person = new Person();
+        boolean expected = true;
+
+        // When
+        person.setAlive(expected);
+
+        // Then
+        boolean actual = person.isAlive();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void testSetPhoneNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "444-555-1212";
+
+        // When
+        person.setPhoneNumber(expected);
+
+        // Then
+        String actual = person.getPhoneNumber();
+        Assert.assertEquals(expected, actual);
+    }
 }
